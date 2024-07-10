ea613b09c4640223984522ce71fb7930bbc35279
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-07-11 00:30:13 +0700

    fix: jose insteadof jwt annd fix some file name

--- src/comDashboard/Page.tsx
+++ src/comDashboard/Page.tsx
@@ -1,7 +1,5 @@
 const ComDashboard = () => {
-    return(
-        <h1>หน้าสำหรับเปิดในคอม</h1>
-    )
-}
+  return <h1>หน้าสำหรับเปิดในคอม</h1>;
+};
 
-export default ComDashboard
+export default ComDashboard;
