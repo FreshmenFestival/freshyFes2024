90e30ee562a4151286bc0c712634cb7b390437f7
oransom48 <kornrawee.kochtat@gmail.com>
2024-07-21 02:09:42 +0700

    constant 0.45 threshold 20

--- src/components/Shake.tsx
+++ src/components/Shake.tsx
@@ -64,7 +64,7 @@ const ShakeComponent: React.FC<ShakeComponentProps> = ({ userData, onShowDashboa
         `Acceleration: x=${x}, y=${y}, z=${z}, total=${acceleration}`
       );
 
-      if (acceleration > 25 && !isShaking) {
+      if (acceleration > 20 && !isShaking) {
         setCount((prevCount) => {
           const nowTick = new Date();
           const newCount = prevCount + 1;
