c55d742bdac52b8e7b5987488165c0b03ae8d7b1
oransom48 <kornrawee.kochtat@gmail.com>
2024-07-22 02:10:17 +0700

    idk

--- src/components/Shake.tsx
+++ src/components/Shake.tsx
@@ -200,7 +200,7 @@ const ShakeComponent: React.FC<ShakeComponentProps> = ({ userData, onShowDashboa
 
         {isPLaying && (
           <div>
-            <div className={`w-30 h-30 rounded-full transition duration-300 ease-in-out ${countChange ? '-translate-y-1 scale-110 bg-indigo-500' : 'bg-blue-600'}`}></div>
+            <div className={`w-30 h-30 bg-blue-600 rounded-full transition duration-300 ease-in-out ${countChange ? '-translate-y-1 scale-110 bg-indigo-500' : ''}`}></div>
             <p>Shake count: {count}</p>
             <button className="mt-4 px-6 py-2 bg-red-500 text-white rounded-full focus:outline-none" onClick={handleStop}>
               Stop
