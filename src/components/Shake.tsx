f2037bbacd330fa42e8e9b68a93c131f21c592af
oransom48 <kornrawee.kochtat@gmail.com>
2024-07-22 04:23:04 +0700

    ping if else

--- src/components/Shake.tsx
+++ src/components/Shake.tsx
@@ -199,7 +199,7 @@ const ShakeComponent: React.FC<ShakeComponentProps> = ({ userData, onShowDashboa
 
         {isPLaying && (
           <div>
-            <div className={`w-20 h-20 ${isCountChange ? 'animate-pingonce' : ''} bg-green-600 rounded-full`}></div>
+            <div className={`${isCountChange ? 'animate-pingonce w-20 h-20 bg-green-600 rounded-full' : ''} `}></div>
             <p>Shake count: {count}</p>
             <button className="mt-4 px-6 py-2 bg-red-500 text-white rounded-full focus:outline-none" onClick={handleStop}>
               Stop
