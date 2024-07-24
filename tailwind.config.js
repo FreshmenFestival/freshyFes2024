2e043f9c3bf1ddb969b5f9dc6e39a7fea7c453cb
ahttan <94210764+meimei2548@users.noreply.github.com>
2024-07-24 11:06:46 +0700

    test shake

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ export default {
       },
       animation: {
         'pingonce': 'ping 1s cubic-bezier(0, 0, 0.2, 1)',
-        'bounceonce': 'bounce 0.2s',
+        'bounceonce': 'bounce 1s',
       },
       backgroundImage: theme => ({
         "phone": "url('/public/Phone.png')",
