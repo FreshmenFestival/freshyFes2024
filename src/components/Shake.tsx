771d39c9197545a63d44f099962b8bee67416306
oransom48 <kornrawee.kochtat@gmail.com>
2024-07-25 18:38:48 +0700

    back to load icon

--- src/components/Shake.tsx
+++ src/components/Shake.tsx
@@ -185,7 +185,7 @@ const ShakeComponent: React.FC<ShakeComponentProps> = ({ userData, onShowDashboa
     <div>
       {loading ? (
         <div className="flex flex-col items-center justify-center h-screen bg-phone">
-          <img className="animate-ping h-24 w-24 rounded-full" src="/logo.png"></img>
+          <img className="animate-ping h-20 w-20" src="/progress_activity.png"></img>
         </div>
       ) : (
         <div className="flex flex-col items-center justify-center h-screen bg-phone font-alice">
