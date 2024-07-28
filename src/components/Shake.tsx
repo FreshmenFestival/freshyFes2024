49711f684563dd25b0d97085a36b5fde71538154
oransom48 <kornrawee.kochtat@gmail.com>
2024-07-28 13:30:47 +0700

    touch end and start

--- src/components/Shake.tsx
+++ src/components/Shake.tsx
@@ -200,7 +200,7 @@ const ShakeComponent: React.FC<ShakeComponentProps> = ({ userData, onShowDashboa
 
         {isPLaying && (
           <div>
-            <button className="mt-4 px-6 py-2 bg-red-500 text-white rounded-full focus:outline-none" onTouchEnd={handleStop}>
+            <button className="mt-4 px-6 py-2 bg-red-500 text-white rounded-full focus:outline-none" onTouchStart={handleStop}>
               stop
             </button>
           </div>
