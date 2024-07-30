61608cf36a998a4539f67bbb5e7c27efc56d5f40
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-07-31 02:34:17 +0700

    feat: log in async

--- src/login/Page.tsx
+++ src/login/Page.tsx
@@ -3,22 +3,22 @@ import { collection, getDocs, query, where } from "firebase/firestore";
 import { db } from "../firebase";
 
 enum Department {
-  MATHCOM = "ภาควิชาคณิตศาสตร์และวิทยาการคอมพิวเตอร์",
-  MARINE = "ภาควิชาวิทยาศาสตร์ทางทะเล",
-  CHEM = "ภาควิชาเคมี",
-  CHEMTECH = "ภาควิชาเคมีเทคนิค",
-  BIO = "ภาควิชาชีววิทยา",
-  BIOCHEM = "ภาควิชาชีวเคมี",
-  BSAC = "หลักสูตรเคมีประยุกต์ (BSAC)",
-  BBTECH = "หลักสูตรเทคโนโลยีชีวภาพ (BBTECH)",
-  FOODTECH = "ภาควิชาเทคโนโลยีทางอาหาร",
-  MATSCI = "ภาควิชาวัสดุศาสตร์",
-  PHYSICS = "ภาควิชาฟิสิกส์",
-  BOTGEN = "ภาควิชาพฤกษศาสตร์",
-  MICROBIO = "ภาควิชาจุลชีววิทยา",
-  PHOTO = "ภาควิชาวิทยาศาสตร์ทางภาพถ่าย",
-  GEO = "ภาควิชาธรณีวิทยา",
-  ENVI = "ภาควิชาวิทยาศาสตร์สิ่งแวดล้อม",
+  MATHCOM = "ภาคคณิตศาสตร์และวิทยาการคอมพิวเตอร์",
+  MARINE = "ภาควิทยาศาสตร์ทางทะเล",
+  CHEM = "ภาคเคมี",
+  CHEMTECH = "ภาคเคมีเทคนิค",
+  BIO = "ภาคชีววิทยา",
+  BIOCHEM = "ภาคชีวเคมี",
+  BSAC = "ภาคเคมีประยุกต์",
+  BBTECH = "ภาคเทคโนโลยีชีวภาพ",
+  FOODTECH = "ภาคเทคโนโลยีทางอาหาร",
+  MATSCI = "ภาควัสดุศาสตร์",
+  PHYSICS = "ภาคฟิสิกส์",
+  BOTGEN = "ภาคพฤกษศาสตร์",
+  MICROBIO = "ภาคจุลชีววิทยา",
+  PHOTO = "ภาควิทยาศาสตร์ทางภาพถ่าย",
+  GEO = "ภาคธรณีวิทยา",
+  ENVI = "ภาควิทยาศาสตร์สิ่งแวดล้อม",
 }
 
 type DepartmentKey = keyof typeof Department;
@@ -31,19 +31,15 @@ const getEnumValues = (enumObj: typeof Department) => {
 };
 
 interface LoginProps {
-  onLogin: (
-    data: { uid: string; group: string; name: string },
-    nickName: string
-  ) => void;
+  onLogin: (data: { uid: string; group: string; name:string; }) => void;
 }
 
 const Login: React.FC<LoginProps> = ({ onLogin }) => {
   const [studentId, setStudentId] = useState("");
-  const [nickName, setName] = useState("");
+  const [name, setName] = useState("");
   const [department, setDepartment] = useState<Department | "">("");
   const [error, setError] = useState("");
   const [errorID, setErrorID] = useState("");
-  const [errorNN, setErrorNN] = useState("");
   const [checking, setChecking] = useState(false);
   const [firstLoad, setFirstLoad] = useState(true);
 
@@ -51,24 +47,20 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
     try {
       const q = query(
         collection(db, "memberlist"),
-        where("uid", "==", '6534435223')
+        where("uid", "==", studentId)
       );
       setChecking(true);
       const querySnapshot = await getDocs(q);
       if (!querySnapshot.empty) {
         const userDoc = querySnapshot.docs[0];
-        const userData = userDoc.data() as {
-          uid: string;
-          group: string;
-          name: string;
-        };
-        onLogin(userData, nickName);
+        const userData = userDoc.data() as { uid: string; group: string; name:string; };
+        onLogin(userData);
       } else {
         setChecking(false);
         if (firstLoad) {
           setFirstLoad(false);
         } else {
-          setError("Noppo! ! ! Try again ;P ");
+          setError("ไม่พบข้อมูล กรุณาลองอีกครั้ง");
         }
       }
     } catch (err) {
@@ -86,24 +78,11 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
 
   const handleIDBlur = () => {
     if (studentId.length !== 10) {
-      setErrorID("incorrect student ID");
+      setErrorID("รหัสนิสิตไม่ถูกต้อง");
       setStudentId("");
     } else {
       setErrorID("");
     }
-  };
-
-  const handleNNBlur = () => {
-    if (nickName.length === 0) {
-      setErrorNN("กลับมากรอกชื่อก่อนสิ");
-      setName("");
-    } else {
-      setErrorNN("");
-    }
-  };
-
-  const isButtonDisabled = () => {
-    return !errorID || !errorNN;
   };
 
   useEffect(() => {
@@ -111,16 +90,15 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center h-screen bg-phone bg-contain ">
-      {checking ? (
+    <div className="flex justify-center items-center min-h-screen bg-phone ">
+      { checking ? (
         <img className="animate-spin h-18 w-18" src="/progress_amber.png"></img>
       ) : (
         <div className="text-amber-900 rounded-2xl  w-80">
-          <h1>for test4</h1>
-          <h1 className="text-center text-3xl font-alice mb-2"><b>Welcome to</b></h1>
-          <h1 className="text-center text-3xl font-alice mb-2"><b>The Myths of Yggdrasil</b></h1>
-          <div className="flex flex-col font-playfair mb-2">
-            <label className="mt-4 block text-base font-alice">Student ID</label>
+          <h1 className="text-center text-4xl font-great mb-2"><b>welcome to</b></h1>
+          <h1 className="text-center text-5xl mb-6 font-great"><b>Yggdrasil</b></h1>
+          <div className="flex flex-col font-alice mb-2">
+            <label className="block text-base">Student ID</label>
             <input
               type="text"
               placeholder="67xxxxxx23"
@@ -133,20 +111,18 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
             {errorID && <p className="text-red-500 text-sm">{errorID}</p>}
           </div>
 
-          <div className="flex flex-col mb-2 font-playfair">
+          <div className="flex flex-col mb-2 font-alice">
             <label className="block text-base ">Name</label>
             <input
               type="text"
               placeholder="ใจ่ใจ๊"
-              value={nickName}
+              value={name}
               onChange={(e) => setName(e.target.value)}
-              onBlur={handleNNBlur}
               className="w-full px-3 py-2 border border-gray-300 rounded-md bg-gray-50 focus:outline-none focus:border-amber-900"
             />
-            {errorNN && <p className="text-red-500 text-sm">{errorNN}</p>}
           </div>
 
-          <div className="flex flex-col mb-2 font-playfair">
+          <div className="flex flex-col mb-2 font-alice">
             <label className="block text-base">Department</label>
             <select
               value={department}
@@ -163,14 +139,15 @@ const Login: React.FC<LoginProps> = ({ onLogin }) => {
               ))}
             </select>
           </div>
+
           <button
             onTouchStart={handleLogin}
-            disabled={isButtonDisabled()}
-            className="mt-4 w-full bg-amber-900 text-white py-2 rounded-md hover:bg-amber-700 transition duration-300 font-playfair"
+            className="w-full bg-yellow-700 text-white py-2 rounded-md hover:bg-amber-900 transition duration-300 font-alice"
           >
-            Accept
+            accept
           </button>
-          {error && <p className="text-red-600 text-sm mt-4">{error}</p>}
+          {error && <p className="text-red-500 text-sm mt-4">{error}</p>}
+
         </div>
       )}
     </div>
