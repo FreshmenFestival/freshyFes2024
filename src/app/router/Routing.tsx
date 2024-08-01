228d87a8db390a19eab34edecaa214a227b7b813
ahttan <94210764+meimei2548@users.noreply.github.com>
2024-08-01 16:58:40 +0700

    closed D3

--- src/app/router/Routing.tsx
+++ src/app/router/Routing.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import App from '../App';
+// import App from '../App';
 import ComDashboard from '../../comDashboard/Page';
 import ClosedPage from '../../components/closed';
 
@@ -10,7 +10,7 @@ function Routing() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<ClosedPage />}  />
         <Route path="/dashboard" element={<ComDashboard />} />
         <Route path="/testclosed" element={<ClosedPage />} />
 
