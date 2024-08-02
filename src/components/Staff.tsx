a1c43139eae72252a9b0fb0b33f946a4e674110a
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-08-02 12:07:27 +0700

    feat: secret

--- src/components/Staff.tsx
+++ src/components/Staff.tsx
@@ -5,7 +5,7 @@ interface StaffProps {
 const GiftStaff: React.FC<StaffProps> = ({ onBack }) => {
     return (
         <div className="flex flex-col justify-center items-center bg-phone bg-contain font-prompt min-h-screen p-4">
-            <img src="/secret/Oat.png" alt="Gift Staff" className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg rounded-md mb-4" />
+            <img src="/secret/staff.jpg" alt="Gift Staff" className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg rounded-md mb-4" />
             <h1 className="text-center text-lg sm:text-xl md:text-2xl">สตาฟทุกคนเก่งมากก</h1>
             <h1 className="text-center text-lg sm:text-xl md:text-2xl">วันนี้วันสุดท้ายแล้ว</h1>
             <h1 className="text-center text-lg sm:text-xl md:text-2xl mb-4">สู้ ๆ ฮ้าฟฟฟ</h1>
