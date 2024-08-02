f6c3eead94784583e084b6f87a122d237ddc895d
ahttan <94210764+meimei2548@users.noreply.github.com>
2024-08-02 17:06:55 +0700

    final closed

--- src/components/closed.tsx
+++ src/components/closed.tsx
@@ -3,7 +3,8 @@ const ClosedPage = () => {
         <div className=" w-screen h-screen flex justify-center items-center bg-phone bg-contain font-prompt">
             <img src="babyTiger.png" className="h-{50px}"/>
             <h2><b>อุ๊ย! หมดเวลาเล่นเกมแล้ว</b></h2>
-            <h2><b>เจอกันพนฮะ ;p</b></h2>
+            <h2><b>ไว้เจอกันโอกาสหน้านะ ;p</b></h2>
+            <h2><b>พี่เสือไปแนะ บะบัยยย</b></h2>
         </div>
     );
 }
