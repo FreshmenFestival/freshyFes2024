955d92c20b3fb8dc7496df5efbb2a8206a6c7a2e
Bunyawat Naunnak <bunyawat@localhost.localdomain>
2024-08-11 16:54:01 +0700

    chore: add bun

--- src/app/router/Routing.tsx
+++ src/app/router/Routing.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import App from '../App';
+import App from '../App';
 import ComDashboard from '../../comDashboard/Page';
-import ClosedPage from '../../components/closed';
-
 
 function Routing() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ClosedPage/>}  />
+        <Route path="/" element={<App/>}  />
         <Route path="/dashboard" element={<ComDashboard />} />
         <Route path="/testclosed" element={<ClosedPage />} />
 
